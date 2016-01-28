0e0ae874b2c79634b66fc55f16f75658eda00d9d
pburgos <pburgos@corpora360.com>
2016-01-28 12:44:45 +0100

    First commit

---
+++ tercio_delta/settings/local.py
@@ -0,0 +1,30 @@
+"""Development settings and globals."""
+
+from __future__ import absolute_import
+
+from .base import *
+
+# DEBUG CONFIGURATION
+# See: https://docs.djangoproject.com/en/dev/ref/settings/#debug
+DEBUG = True
+# END DEBUG CONFIGURATION
+
+
+# EMAIL CONFIGURATION
+# See: https://docs.djangoproject.com/en/dev/ref/settings/#email-backend
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+# END EMAIL CONFIGURATION
+
+# DATABASE CONFIGURATION
+# See: https://docs.djangoproject.com/en/dev/ref/settings/#databases
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': join(SITE_ROOT, 'db.sqlite3'),
+    },
+}
+# END DATABASE CONFIGURATION
+
+# http://django-debug-toolbar.readthedocs.org/en/latest/installation.html
+INTERNAL_IPS = ('127.0.0.1',)
+# END TOOLBAR CONFIGURATION
